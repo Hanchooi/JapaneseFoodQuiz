e5de6dfc6e3eea3f2de6ad316643cc57b4e02a78
Han <tsinchooi@gmail.com>
2020-05-15 20:13:45 +0800

    Setting User Info

--- X2 Project2 Agile Web /app/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config(object):
-
-    SECRET_KEY = os.environ.get('SERCRET_KEY') or "123-key"
--- X2 Project2 Agile Web /config.py
+++ X2 Project2 Agile Web /config.py
@@ -1,5 +1,12 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
 
+    # Creation of secret key for WTForms
     SECRET_KEY = os.environ.get('SERCRET_KEY') or "123-key"
+
+    # Set specs for SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
