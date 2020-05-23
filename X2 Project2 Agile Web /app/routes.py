545ffe7ac4b36fb01e78329f10deca44bc0b7a03
Su <chadwick.su.unsw@gmail.com>
2020-05-23 11:58:18 +0800

    routes.py change

--- X2 Project2 Agile Web /app/routes.py
+++ X2 Project2 Agile Web /app/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template, flash, redirect
-from app import app
-from app.forms import LoginForm, AdminForm, SignUpForm, PasswordForm, NameForm
+from flask import render_template, flash, redirect,url_for
+from app import app, db
+from app.forms import LoginForm, AdminForm, SignUpForm, PasswordForm, NameForm, RegistrationForm
+from app.models import User
+from flask_login import login_user, logout_user, current_user, login_required
 
 
 @app.route('/')
@@ -62,12 +64,14 @@ def admin_login():
 @app.route('/user_login', methods=['GET', 'POST'])
 def user_login():
 
-    form = LoginForm()
+    form = SignUpForm()
 
     if form.validate_on_submit():
-        flash("Login requested for User ID {}, remember_me={}, Password {}".format(
-            form.userID.data, form.remember_me.data, form.password.data
-        ))
+        user = User.query.filter_by(userID=form.userID.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect('/user_page')
 
     return render_template('user_login.html', form=form)
@@ -75,16 +79,17 @@ def user_login():
 
 @app.route('/user_register', methods=['GET', 'POST'])
 def user_signUp():
-
-    form = SignUpForm()
-
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
     if form.validate_on_submit():
-        flash("Sign Up requested from userID {}, remember_me={}, Password {}, Display Name{} ".format(
-            form.userID.data, form.remember_me.data, form.password.data, form.displayName.data
-        ))
-        return redirect('/user_page')
-
-    return render_template('user_register.html', form=form)
+        user = User(userId=form.userID.data, username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/user_change_password', methods=['GET', 'POST'])
@@ -139,3 +144,8 @@ def user_change_name():
         return redirect('/user_page')
 
     return render_template('change_name.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
