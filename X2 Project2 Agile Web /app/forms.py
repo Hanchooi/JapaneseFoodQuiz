6fb7e5133e06a1cdb96260d5fa7560ff3552599a
Su <chadwick.su.unsw@gmail.com>
2020-05-23 23:47:18 +0800

    forms and routes changes

--- X2 Project2 Agile Web /app/forms.py
+++ X2 Project2 Agile Web /app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, FileField, SelectField
 from wtforms.validators import DataRequired
-from app.models import User
+from app.models import User, QuizSet
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
 class LoginForm(FlaskForm):
@@ -27,17 +27,19 @@ class AdminForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
+
+
 class PasswordForm(FlaskForm):
     
     userID = StringField('ID:', validators=[DataRequired()])
     oldPassword = PasswordField('Old Password:', validators=[DataRequired()])
     newPassword = PasswordField('New Password:', validators=[DataRequired()])
-    retypePassword = PasswordField('Retype Password:', validators=[DataRequired()])
+    retypePassword = PasswordField('Retype Password:', validators=[DataRequired(), EqualTo('newPassword')])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Change Password')
 
-class NameForm(FlaskForm):
 
+class NameForm(FlaskForm):
     userID = StringField('ID:', validators=[DataRequired()])    
     newDisplayName = StringField('New Display Name:', validators=[DataRequired()])
     submit = SubmitField('Change Name')
@@ -47,16 +49,44 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    retypePassword = PasswordField(
+        'RetypePassword', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
-    def validate_username(self, userID):
-        user = User.query.filter_by(userID=userID.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different username.')
+    def validate_userID(self, userID):
+        user = User.query.filter_by(id=userID.data).first()
+        if user is not None :
+            raise ValidationError('Please use a different Userid.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+class UploadQuizFrom(FlaskForm):
+    quizName = StringField('Quiz Name', validators=[DataRequired()])
+    quizDescription = StringField('Quiz Description ', validators=[DataRequired()])
+    picture = FileField('select picture',  validators=[DataRequired()])
+    submit = SubmitField('upload')
+
+
+class QuestionFrom(FlaskForm):
+    quizSets = QuizSet.query.all()
+    index = 1
+    choices = []
+    for quizSet in quizSets:
+        choices.append((index, quizSet.quizSetID))
+        index += 1
+    quizSetId = SelectField('quiz set id :', validators=[DataRequired('')],
+                            choices=choices, default=1)
+    question = StringField('Question :', validators=[DataRequired()])
+    choiceA = StringField('choice A :', validators=[DataRequired()])
+    choiceB = StringField('choice B :', validators=[DataRequired()])
+    choiceC = StringField('choice C :')
+    choiceD = StringField('choice D :')
+    submit = SubmitField('upload')
+
+class AnswerForm(FlaskForm):
+    answer = StringField('Answer :', validators=[DataRequired()])
+    submit = SubmitField('submit')
+
--- X2 Project2 Agile Web /app/routes.py
+++ X2 Project2 Agile Web /app/routes.py
@@ -1,9 +1,10 @@
 from flask import render_template, flash, redirect, url_for,request
 from app import app, db
-from app.forms import LoginForm, AdminForm, SignUpForm, PasswordForm, NameForm, RegistrationForm, UploadQuizFrom
-from app.models import User, QuizSet
+from app.forms import LoginForm, AdminForm, SignUpForm, PasswordForm, NameForm,\
+RegistrationForm, UploadQuizFrom,QuestionFrom, AnswerForm
+from app.models import User, QuizSet, Question, Answer
 from flask_login import login_user, logout_user, current_user, login_required
-
+import base64
 
 @app.route('/')
 @app.route('/index')
@@ -13,51 +14,145 @@ def index():
 
 @app.route('/qs')
 def qs():
-    return render_template('qs.html')
+    quizSets = QuizSet.querry.all()
+    return render_template('qs.html', quizSets=quizSets, base64=base64)
 
 
-@app.route('/qa')
+@app.route('/qa', methods=['GET', 'POST'])
 def qa():
-    return render_template('qa.html')
+    form = AnswerForm()
+    quizSetId = request.get_data('id')
+    index = request.get_data('index')
+    question = None
+    total_count = 0
+    if quizSetId is not None:
+        questions = Question.query.all()
+        total_count = len(questions)
+        if len(questions) > 0:
+            if index is None:
+                index = 0
+                question = questions[0]
+            elif index < len(questions):
+                question = questions[index]
+    if form.validate_on_submit():
+        if question is not None:
+            user = current_user
+            answer = Answer.query(userID=user.id, quizSetId=quizSetId)
+            if answer is None:
+                answer = Answer(user.id, quizSetId)
+                db.session.add(answer)
+                db.session.commit()
+            record = answer.correctNumber.split(",")
+            if form.answer.data.lower() == question.correctAnswer.lower():
+                if str(question.questionID) not in record:
+                    record.append(str(question.questionID))
+            else:
+                if str(question.questionID) not in record:
+                    record.remove(str(question.questionID))
+            answer.totalNumber = total_count
+            if len(record) > 0:
+                answer.correctNumber = ",".join(record)
+            else:
+                answer.correctNumber = ""
+            db.session.commit()
+    return render_template('qa.html', form=form, question=question, index=index, quizSetId=quizSetId)
+
 
 @login_required
 @app.route('/manage_quiz')
 def manage_quiz():
-    return render_template('manage_quiz.html')
+    quizSets = QuizSet.querry.all()
+    return render_template('manage_quiz.html', quizSets=quizSets)
+
+
+@app.route('/delete_quiz_set')
+def delete_quiz_set():
+    user = current_user
+    if user.name == 'admin':
+        quizSetId = request.get_data('id')
+        if quizSetId is not None:
+            quizSet = QuizSet.query.filter(quizSetID=quizSetId).first()
+            db.session.delete(quizSet)
+            db.session.commit()
+    return redirect('/manage_user')
+
+@app.route('/update')
+def update():
+    user = current_user
+    if user.name == 'admin':
+        userID = request.get_data('id')
+        if userID is not None:
+            user = User.query.filter(id=userID).first()
+            db.session.delete(user)
+            db.session.commit()
+    return redirect('/manage_user')
+
+
+@app.route('/upload_quiz')
+def upload_quiz():
+    form = QuestionFrom()
+    if form.validate_on_submit():
+        choice = request.form.get('choice')
+        question = Question(int(form.quizSetId.data), form.question.data, form.choiceA.data,
+                            form.choiceB.data, form.choiceC.data, form.choiceD.data, choice)
+        db.session.add(question)
+        db.session.commit()
+    return render_template('upload_quiz.html', form=form)
 
 
 @login_required
 @app.route('/manage_user')
 def manage_user():
-    return render_template('manage_user.html')
+    users = User.query.filter(User.name!="admin")
+    return render_template('manage_user.html', users=users)
 
 
-@login_required
-@app.route('/upload_question_set')
+@app.route('/upload_question_set', methods=['GET', 'POST'])
 def upload_question_set():
-    return render_template('upload_question_set.html')
-
-
-@login_required
-@app.route('/upload_quiz',methods=['GET', 'POST'])
-def upload_quiz():
     form = UploadQuizFrom()
+    user = current_user
     if form.validate_on_submit():
-        file = request.files['file'].read()
-        quizSet = QuizSet(name=form.quizName.data, description=form.quizDescription.data, picture=file)
+        file = request.files['picture'].read()
+        quizSet = QuizSet(name=form.quizName.data, description=form.quizDescription.data, picture=file, userID=user.id)
         db.session.add(quizSet)
         db.session.commit()
-    return render_template('upload_quiz.html')
+        return redirect('/user_page')
+    return render_template('upload_question_set.html', form=form)
 
 
 @login_required
 @app.route('/user_page')
 def user_page():
     user = current_user
+    print(user.id)
+    answers = Answer.query.all()
+    show_answers = []
+    if answers and len(answers) > 0:
+        for answer in answers:
+            if answer.userID != user.id:
+                continue
+            record = answer.correctNumber.split(",")
+            quiz_set = QuizSet.query.filter(quizSetID=answer.quizSetID).first()
+            if quiz_set:
+                score = str(len(record))+"/"+str(len(answer.totalNumber))
+                show = {"quiz_set_id": answer.quizSetID, "name": quiz_set.name, "socre": score}
+                show_answers.append(show)
+    quiz_sets = QuizSet.query.all()
+    show_quizs = []
+    if quiz_sets and len(quiz_sets) > 0:
+        for quiz_set in quiz_sets:
+            if quiz_set.userID != user.id:
+                continue
+            q_answers = Answer.query.filter_by(quizSetID=int(quiz_set.quizSetID)).all()
+            otherNumber = 0
+            if q_answers is not None:
+                otherNumber = len(q_answers)
+            show = {"quiz_set_id": quiz_set.quizSetID, "name": quiz_set.name, "otherNumber": otherNumber}
+            show_quizs.append(show)
     if user.status == "admin":
         return render_template('user_page.html', username=current_user.name)
     else:
-        return render_template('user_page.html', username=current_user.name)
+        return render_template('user_page.html', username=current_user.name, show_answers=show_answers, show_quizs=show_quizs)
 
 
 @app.route('/admin', methods=['GET', 'POST'])
@@ -70,7 +165,7 @@ def admin_login():
             flash("Invalid userID or password")
             return redirect(url_for('user_login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect('/manage_user', username=current_user.name)
+        return redirect('/manage_user')
 
     return render_template('admin_login.html', form=form)
 
@@ -101,6 +196,7 @@ def user_signUp():
         return redirect(url_for('user_login'))
     return render_template('user_register.html', title='Register', form=form)
 
+
 @login_required
 @app.route('/user_change_password', methods=['GET', 'POST'])
 def user_change_password():
@@ -113,6 +209,7 @@ def user_change_password():
         return redirect('/user_page')
 
     return render_template('change_password.html', form=form)
+
 
 @login_required
 @app.route('/admin_change_password', methods=['GET', 'POST'])
@@ -127,19 +224,6 @@ def admin_change_password():
 
     return render_template('change_password.html', form=form)
 
-@login_required
-@app.route('/admin_change_name', methods=['GET', 'POST'])
-def admin_change_name():
-
-    form = NameForm()
-
-    if form.validate_on_submit():
-        flash("Name change requested from adminID {} to newDisplayName {}".format(
-            form.userID.data, form.newDisplayName.data
-        ))
-        return redirect('/manage_quiz')
-
-    return render_template('change_name.html', form=form)
 
 @login_required
 @app.route('/user_change_name', methods=['GET', 'POST'])
@@ -153,6 +237,7 @@ def user_change_name():
         return redirect('/user_page')
 
     return render_template('change_name.html', form=form)
+
 
 @app.route('/logout')
 def logout():
